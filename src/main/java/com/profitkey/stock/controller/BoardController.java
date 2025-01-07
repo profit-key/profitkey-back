c5322c5f30591b02aa57b319ae2aa5633f62049d
‘jayce8887@gmail.com’ <kjw88887@gmail.com>
2025-01-07 21:31:04 +0900

    PROF-7 [Docs] : 스웨거 명세서 작성 예시

--- src/main/java/com/profitkey/stock/controller/BoardController.java
+++ src/main/java/com/profitkey/stock/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.profitkey.stock.controller;
 import com.profitkey.stock.domain.dto.BoardDto;
 import com.profitkey.stock.domain.response.BoardListResponse;
 import com.profitkey.stock.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +18,10 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    // 게시판 목록조회
+    @Operation(summary = "게시판 목록조회",
+        description = """
+                        게시판 목록을 목록을 조회한다!!!!!!!
+                        """)
     @GetMapping
     public ResponseEntity<List<BoardListResponse>> getBoards() {
         List<BoardListResponse> boards = boardService.getAllBoards();

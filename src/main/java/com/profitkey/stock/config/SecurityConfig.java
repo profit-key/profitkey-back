4ac2c99b630e22459b27815a946179a11c8b8d66
김민주 <tptlffldk97@naver.com>
2025-01-12 19:46:36 +0900

    chore: 스프링 시큐리티 재설정 후 업로드

--- src/main/java/com/profitkey/stock/config/SecurityConfig.java
+++ src/main/java/com/profitkey/stock/config/SecurityConfig.java
@@ -22,20 +22,20 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/images/**", "/js/**").permitAll()
+                        .requestMatchers("/login", "/images/**", "/js/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()  // Swagger 경로 예외 추가
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .defaultSuccessUrl("/", true)
                         .permitAll()
                 )
-                .logout(logout -> logout.permitAll())
+                .logout(logout -> logout.permitAll());
 
-                // OAuth2 로그인 설정 추가
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login")  // 로그인 페이지 URL 설정 (선택 사항)
-                        .permitAll()  // OAuth2 로그인 페이지에 대한 접근 허용
-                );
+        // OAuth2 로그인 설정 주석 처리된 부분은 다시 활성화하면 됩니다.
+        //.oauth2Login(oauth2 -> oauth2
+        //      .loginPage("/login")
+        //      .permitAll()
+        //);
 
         return http.build();
     }

30b5b46c8b846f40dac9fca22b07ab4367489a19
jayce <kin88887@gmail.com>
2025-01-24 17:51:33 +0900

    PROF-22 [Feat] : http get/post api 요청 공통화

--- src/main/java/com/profitkey/stock/controller/StockController.java
+++ src/main/java/com/profitkey/stock/controller/StockController.java
@@ -1,11 +1,14 @@
 package com.profitkey.stock.controller;
 
 import com.profitkey.stock.dto.KisApiProperties;
+import com.profitkey.stock.dto.request.stock.InquirePriceRequest;
 import com.profitkey.stock.service.StockService;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +26,27 @@ public class StockController {
 		return stockService.getStockInfo();
 	}
 
+	/**
+	 * OAuth인증
+	 * 접근토큰발급(P)[인증-001]
+	 * * 1분에 하나씩 발급가능
+	 * @param
+	 * @return access_token
+	 */
 	@GetMapping("/getToken")
 	public String getToken() throws IOException {
 		return stockService.getToken();
 	}
 
-	@GetMapping("/getInquirePrice")
-	public String getInquirePrice() throws IOException {
-		return stockService.getInquirePrice();
+	/**
+	 * [국내주식]기본시세 API CALL
+	 * 주식현재가 시세[v1_국내주식-008]
+	 * @param request 조회할 주식 입력조건
+	 * @return 주식데이터
+	 */
+	@PostMapping("/getInquirePrice")
+	public String getInquirePrice(@RequestBody InquirePriceRequest request) {
+		return stockService.getInquirePrice(request);
 	}
 
 }

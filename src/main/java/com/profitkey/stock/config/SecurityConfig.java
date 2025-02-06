00cb588a8cf072b052f37581d20bf9683be200f1
hyu-x-ck24 <hyu.x.ck24@gmail.com>
2025-02-06 19:31:42 +0900

    test: 주식API 403에러 디버깅 테스트2

--- src/main/java/com/profitkey/stock/config/SecurityConfig.java
+++ src/main/java/com/profitkey/stock/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(
-			List.of("http://localhost:3000", "http://localhost:5173", "https://dev.profitkey.click"));
+			List.of("http://localhost:3000", "http://localhost:5173", "https://dev-server.profitkey.click"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowCredentials(true);

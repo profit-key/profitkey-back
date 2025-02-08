389ebae87ff65d850f62331517da0eafba264831
김민주 <tptlffldk97@naver.com>
2025-02-09 01:55:13 +0900

    PROF-83 [feat]: 프로필 이미지 수정 기능 추가

---
+++ src/main/java/com/profitkey/stock/dto/response/mypage/UserInfoResponse.java
@@ -0,0 +1,19 @@
+package com.profitkey.stock.dto.response.mypage;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoResponse {
+
+	@Schema(description = "사용자 이메일", example = "user@example.com")
+	private String email;
+
+	@Schema(description = "사용자 닉네임", example = "햄스터")
+	private String nickname;
+
+	@Schema(description = "프로필 이미지 URL", example = "https://example.com/profile.jpg")
+	private String profileImageUrl;
+}

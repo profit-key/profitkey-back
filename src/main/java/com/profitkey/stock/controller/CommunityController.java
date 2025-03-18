20a9fb42feef19530a68b910a48c2038ba349317
김민주 <tptlffldk97@naver.com>
2025-03-18 20:34:19 +0900

    PROF-173 [feat]: 댓글/대댓글 조회 api writer정보 반환되도록 수정

--- src/main/java/com/profitkey/stock/controller/CommunityController.java
+++ src/main/java/com/profitkey/stock/controller/CommunityController.java
@@ -2,8 +2,6 @@ package com.profitkey.stock.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +18,6 @@ import com.profitkey.stock.dto.request.community.CommunityRequest;
 import com.profitkey.stock.dto.request.community.CommunityUpdateRequest;
 import com.profitkey.stock.dto.request.community.LikeRequest;
 import com.profitkey.stock.dto.response.community.CommunityResponse;
-import com.profitkey.stock.entity.Community;
 import com.profitkey.stock.service.CommunityService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -115,17 +112,17 @@ public class CommunityController {
 	}
 
 	// 최신순 정렬 API
-	@GetMapping("/latest")
-	@Operation(summary = SwaggerDocs.SUMMARY_COMMUNITY_LATEST, description = SwaggerDocs.DESCRIPTION_COMMUNITY_LATEST)
-	public ResponseEntity<Page<CommunityResponse>> getLatestComments(
-		@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
-
-		Page<Object[]> result = communityService.getLatestComments(pageable);
-		Page<CommunityResponse> response = result.map(obj ->
-			CommunityResponse.fromEntity((Community)obj[0], (long)obj[1], 0L)
-		);
-		return ResponseEntity.ok(response);
-	}
+	// @GetMapping("/latest")
+	// @Operation(summary = SwaggerDocs.SUMMARY_COMMUNITY_LATEST, description = SwaggerDocs.DESCRIPTION_COMMUNITY_LATEST)
+	// public ResponseEntity<Page<CommunityResponse>> getLatestComments(
+	// 	@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+	//
+	// 	Page<Object[]> result = communityService.getLatestComments(pageable);
+	// 	Page<CommunityResponse> response = result.map(obj ->
+	// 		CommunityResponse.fromEntity((Community)obj[0], (long)obj[1], 0L)
+	// 	);
+	// 	return ResponseEntity.ok(response);
+	// }
 
 	// 인기순 정렬 API
 	@GetMapping("/popular")
